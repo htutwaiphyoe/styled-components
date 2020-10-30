4127c906adfff868325dd43fa87bc62d261c6b27
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-30 22:38:15 +0630

    Connection with Redux Devtool Finished

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import reducers from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={createStore(reducers)}>
+        <Provider store={store}>
             <App />
         </Provider>
     </React.StrictMode>,
